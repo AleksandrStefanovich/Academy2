6584d45f2c186783793d356da8957e8692428e4e
AleksandrStefanovich <rodger6699@gmail.com>
2020-07-27 18:26:22 +0300

    added Daos/Dtos, Test

---
+++ HW2/src/test/java/by/academy/ExpensesDaoImplTest.java
@@ -0,0 +1,106 @@
+package by.academy;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlConnection;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class ExpensesDaoImplTest {
+    private ExpensesDaoImpl expensesDao;
+    private IDatabaseConnection connection;
+
+    @Before
+    public void setUp() throws Exception {
+        try {
+            expensesDao = (ExpensesDaoImpl) ExpensesDaoFactory.getExpensesDao("mysql_test");
+            connection = new MySqlConnection(MySqlDataSource.getTestConnection(), "listexpenses_test");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        connection.close();
+        expensesDao = null;
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void read() throws DatabaseUnitException, SQLException {
+        //Given
+        IDataSet dataSet = new FlatXmlDataSetBuilder()
+                .build(ExpensesDaoImplTest.class
+                        .getResourceAsStream("ExpensesDaoImplTest.xml"));
+        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+        ExpensesDto expensesDtoTest = new ExpensesDto();
+        expensesDtoTest.setNum(2);
+        expensesDtoTest.setPaydate(Date.valueOf("2004-03-03"));
+        expensesDtoTest.setReceiver(2);
+        expensesDtoTest.setValue(13);
+        //When
+        ExpensesDto expensesDto = expensesDao.read(2);
+        //Then
+        assertEquals(expensesDto,expensesDtoTest);
+        DatabaseOperation.DELETE.execute(connection, dataSet);
+    }
+
+    @Test
+    public void readAll() {
+    }
+
+    @Test
+    public void update() throws DatabaseUnitException, SQLException {
+        //Given
+        IDataSet dataSet = new FlatXmlDataSetBuilder()
+                .build(ExpensesDaoImplTest.class
+                        .getResourceAsStream("ExpensesDaoImplTest.xml"));
+        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+        ExpensesDto expensesDtoTest = new ExpensesDto();
+        expensesDtoTest.setNum(2);
+        expensesDtoTest.setPaydate(Date.valueOf("2005-03-03"));
+        expensesDtoTest.setReceiver(3);
+        expensesDtoTest.setValue(16);
+        //When
+        expensesDao.update(expensesDtoTest);
+        ExpensesDto expensesDto = expensesDao.read(2);
+        //Then
+        assertEquals(expensesDto,expensesDtoTest);
+        DatabaseOperation.DELETE.execute(connection, dataSet);
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getMaxNum() throws SQLException, DatabaseUnitException {
+        //Given
+        IDataSet dataSet = new FlatXmlDataSetBuilder()
+                .build(ExpensesDaoImplTest.class
+                        .getResourceAsStream("ExpensesDaoImplTest.xml"));
+        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+
+        //When
+        int maxNum = expensesDao.getMaxNum();
+
+        //Then
+        assertEquals(maxNum, 5);
+        DatabaseOperation.DELETE.execute(connection, dataSet);
+    }
+}
